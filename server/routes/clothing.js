05a7526785d6898d9b63401a8012e9a6e2002e0b
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-04-19 18:45:23 +0530

    Promises Approach!

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -4,26 +4,32 @@ const datafile = "server/data/clothing.json";
 const router = express.Router();
 
 /* GET all clothing */
-const fetchClothingData = (callback) => {
-  fs.readFile(datafile, "utf8", function handleFetching(err, data) {
-    if (err) {
-      callback(err, null);
-    } else {
-      const clothingData = JSON.parse(data);
-      callback(err, clothingData);
-    }
+const fetchClothingData = () => {
+  return new Promise(function handlePromise(resolve, reject) {
+    fs.readFile(datafile, "utf8", function handleFetching(err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        const clothingData = JSON.parse(data);
+        resolve(clothingData);
+      }
+    });
   });
 };
 
 router.route("/").get(function handleClothingRoute(req, res) {
-  fetchClothingData(function handleFetchingClothing(err, data) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Returning clothing data.");
+  fetchClothingData()
+    .then(function handleSuccess(data) {
+      console.log("Success Returning clothing data.");
       res.send(data);
-    }
-  });
+    })
+    .catch(function handleError(err) {
+      console.log("Error Returning clothing data.");
+      res.status(500).send(err);
+    })
+    .finally(function handleFinally() {
+      console.log("All done processing Promises.");
+    });
   console.log("Doing more work.");
 });
 
