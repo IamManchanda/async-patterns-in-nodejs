827c64af1b2abdf736266f642254bf6b869e4bef
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-04-19 17:42:08 +0530

    Callback Approach: Async Patterns in Node!

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -1,17 +1,30 @@
-const express = require('express');
-const fs = require('fs');
-const datafile = 'server/data/clothing.json';
+const express = require("express");
+const fs = require("fs");
+const datafile = "server/data/clothing.json";
 const router = express.Router();
 
 /* GET all clothing */
-router.route('/')
-  .get(function(req, res) {
-
-    let rawData = fs.readFileSync(datafile, 'utf8');
-    let clothingData = JSON.parse(rawData);
-
-    res.send(clothingData);
-
+const fetchClothingData = (callback) => {
+  fs.readFile(datafile, "utf8", function handleFetching(err, data) {
+    if (err) {
+      callback(err, null);
+    } else {
+      const clothingData = JSON.parse(data);
+      callback(err, clothingData);
+    }
   });
+};
+
+router.route("/").get(function handleClothingRoute(req, res) {
+  fetchClothingData(function handleFetchingClothing(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Returning clothing data.");
+      res.send(data);
+    }
+  });
+  console.log("Doing more work.");
+});
 
 module.exports = router;
